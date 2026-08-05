8f70cb5e25dd43281c40cd5717b61946944cfd4f
Michael Buchoff <mbuchoff@gmail.com>
2026-08-05 16:58:43 -0700

    test: keep auth route test out of app bundle

--- src/features/account/AuthCallbackRoute.test.tsx
+++ src/features/account/AuthCallbackRoute.test.tsx
@@ -1,6 +1,6 @@
 import { render, waitFor } from '@testing-library/react-native';
 
-import AuthCallbackRoute from './callback';
+import AuthCallbackRoute from '../../../app/auth/callback';
 
 const mockNavigateBack = jest.fn();
 
